791e4dd33dd208b7b1de27dcf2295aaa9f263d2a
Amir Ibrahim <48657559+amiribrahimkholif@users.noreply.github.com>
2021-10-20 18:38:37 +0200

    Intial Commit after adding intial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorComponent } from './author/author.component';
+import { BookComponent } from './book/book.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path:'/',redirectTo:'/home',pathMatch:'full',},
+  {path:'home',component: HomeComponent},
+  {path:'book',component: BookComponent},
+  {path:'author',component: AuthorComponent},
+  {path:'**',component: NotFoundComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
+import { AuthorComponent } from './author/author.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookComponent,
+    AuthorComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
